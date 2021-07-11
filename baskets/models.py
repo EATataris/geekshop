88ae08d4fb109560bcc31531d4eee6ecaa51aff2
Sasuke <emir-ali818@mail.ru>
2021-07-11 14:22:25 +0300

    add total_sum and total_quantity basket methods

--- baskets/models.py
+++ baskets/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.db.models import Sum, Count
 
 from users.models import User
 from products.models import Product
 # Create your models here.
+
 
 class Basket(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -15,3 +17,11 @@ class Basket(models.Model):
 
     def sum(self):
         return self.quantity * self.product.price
+
+    def total_sum(self):
+        baskets = Basket.objects.filter(user=self.user)
+        return  sum(basket.sum() for basket in baskets)
+
+    def total_quantity(self):
+        baskets = Basket.objects.filter(user=self.user)
+        return sum(basket.quantity for basket in baskets)
