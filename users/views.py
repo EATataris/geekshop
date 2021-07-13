9f041dbfc7b1d898f64bbdb53adf54d25662f415
Sasuke <emir-ali818@mail.ru>
2021-07-13 21:29:29 +0300

    add login required for controllers

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileFrom
+from django.contrib.auth.decorators import login_required
+
 from baskets.models import Basket
 from django.contrib import auth, messages
 from django.urls import reverse
@@ -44,7 +46,7 @@ def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('index'))
 
-
+@login_required
 def profile(request):
     if request.method == 'POST':
         form = UserProfileFrom(instance=request.user, files=request.FILES, data=request.POST)
