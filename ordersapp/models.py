37d6ae2e641f8813b413b7d0e5fd9c4256d39e04
Sasuke <emir-ali818@mail.ru>
2021-11-08 23:08:00 +0300

    Add with teg cache method

--- ordersapp/models.py
+++ ordersapp/models.py
@@ -29,13 +29,20 @@ class Order(models.Model):
     def __str__(self):
         return f'Текущий заказ: {(self.pk)}'
 
-    def get_total_quantity(self):
+    def get_total_summary(self):
         items = self.orderitems.select_related()
-        return sum(list(map(lambda x: x.quantity, items)))
+        return {
+            'get_total_cost': sum(list(map(lambda x: x.get_product_cost(), items))),
+            'get_total_quantity': sum(list(map(lambda x: x.quantity, items))),
+        }
 
-    def get_total_cost(self):
-        items = self.orderitems.select_related()
-        return sum(list(map(lambda x: x.get_product_cost(), items)))
+    # def get_total_quantity(self):
+    #     items = self.orderitems.select_related()
+    #     return sum(list(map(lambda x: x.quantity, items)))
+    #
+    # def get_total_cost(self):
+    #     items = self.orderitems.select_related()
+    #     return sum(list(map(lambda x: x.get_product_cost(), items)))
 
     def get_items(self):
         pass
